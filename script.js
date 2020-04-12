a5b8faffc69f58892549b2d01569740c399b2773
rachels242 <47482579+rachels242@users.noreply.github.com>
2020-04-11 21:21:44 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$("button").click(function(event) {
+    $("ul").append(
+      "<li>" +
+      [""][Math.floor(Math.random())] + "</li>"
+    );
+  });
+
+    $("ul").on("click", ".shopping-item-toggle", function() {
+      $(this).parent().parent().find('.shopping-item').toggleClass("shopping-item__checked");
+  });
+
+
+  $("ul").on("click", ".shopping-item-delete", function() {
+      event.preventDefault();
+    $(event.target).closest("li").remove();
+  });
